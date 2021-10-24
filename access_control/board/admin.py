ce3bea8ae9b040ed7c7444ee39afd437ada5aba1
skskdmsdl <skskdmsdl@gmail.com>
2021-10-24 19:17:34 +0900

    모델 등록 및 템플릿 작성

--- access_control/board/admin.py
+++ access_control/board/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Board
 
 # Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('guest_name',)
+
+admin.site.register(Board, BoardAdmin)
--- access_control/board/models.py
+++ access_control/board/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Board(models.Model):
+    start_day = models.DateField(verbose_name='시작날짜')
+    end_day = models.DateField(verbose_name='종료날짜')
+    company = models.CharField(max_length=128,
+                                verbose_name='업체명')
+    position = models.CharField(max_length=128,
+                                verbose_name='직책')
+    guest_name = models.CharField(max_length=32,
+                                verbose_name='출입자명')
+
+    def __str__(self):
+        return self.guest_name
+
+    class Meta:
+        db_table = 'board'
+        verbose_name = '출입명단'
+        verbose_name_plural = '출입명단' 
