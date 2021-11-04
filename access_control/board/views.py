362f1dfe433afb3fef4167fb156cd46fe2010179
skskdmsdl <skskdmsdl@gmail.com>
2021-11-04 21:56:00 +0900

    엑셀 버튼 변경 및 엑셀 표에 테두리 추가

--- access_control/board/views.py
+++ access_control/board/views.py
@@ -30,26 +30,34 @@ def excel_export(request):
     row_num = 0
     col_names = ['번호', '날짜', '업체명', '직책', '이름']
     
+    # 테두리 설정
+    style = xlwt.XFStyle()
+    borders = xlwt.Borders()
+    borders.left = 1
+    borders.right = 1
+    borders.top = 1
+    borders.bottom = 1
+    style.borders = borders
+
     #열이름을 첫번째 행에 추가 시켜준다.
     for idx, col_name in enumerate(col_names):
-    	ws.write(row_num, idx, col_name)
+    	ws.write(row_num, idx, col_name, style)
         
-    
     #데이터 베이스에서 유저 정보를 불러온다.
     # rows = Board.objects.all().values_list('start_date', 'company', 'position', 'guest_name') 
     rows = Board.objects.filter(start_date__gte = today, end_date__lte = today).values_list('start_date', 'start_date', 'company', 'position', 'guest_name')
-
+    
     # 날짜 서식으로 변경
     date_format = xlwt.XFStyle()
+    date_format.borders = borders
     date_format.num_format_str = 'yyyy-mm-dd (aaa)'
-
 
     #유저정보를 한줄씩 작성한다.
     for row in rows:
         row_num +=1
         for col_num in range(len(row)):
-            ws.write(row_num, col_num, row[col_num])
-            ws.write(row_num, 0, row_num)
+            ws.write(row_num, col_num, row[col_num], style)
+            ws.write(row_num, 0, row_num, style)
             ws.write(row_num, 1, todayValue, date_format)
             ws.col(1).width = 25*255
             
